1ea52491d59846e0884876f549c39009f8e20576
Danny Nguyen <giabnguyen1@gmail.com>
2019-08-25 17:07:40 -0400

    project filter
    Starting on filter project, converted html code into javascript. Uploaded images to imgur.

---
+++ js/data.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+        image: {src: "https://i.imgur.com/h3rQZah.png", alt: "First temp image", style: "width:100%; max-width:300px;"}
+    },
+    {
+        image: {src: "https://i.imgur.com/igQwUBl.png", alt: "Second temp image", style: "width:100%; max-width:300px;"}
+    },
+    {
+        image: {src: "https://i.imgur.com/1U1aoXS.jpg", alt: "Third temp image", style: "width:100%; max-width:300px;"}
+    },
+    {
+        image: {src: "https://i.imgur.com/86pJ4bG.jpg", alt: "Third temp image", style: "width:100%; max-width:300px;"}
+    },  
+    {
+        image: {src: "https://i.imgur.com/QdhBRIY.jpg", alt: "Third temp image", style: "width:100%; max-width:300px;"}
+    },  
+    {
+        image: {src: "https://i.imgur.com/Qxow1Av.jpg", alt: "Third temp image", style: "width:100%; max-width:300px;"}
+    },
+      {
+        image: {src: "https://i.imgur.com/71aDtJO.jpg", alt: "Third temp image", style: "width:100%; max-width:300px;"}
+    },
+
+    
+];
+    
